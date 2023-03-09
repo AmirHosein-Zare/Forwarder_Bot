7896612aa193bd52a7ebcd293a93a03fcb723b84
AmirHosein <awir.software@gmail.com>
2023-03-09 18:35:58 +0330

    add forwarder function

--- 01X01/01X01.py
+++ 01X01/01X01.py
@@ -10,4 +10,11 @@ dispatcher = updater.dispatcher
 # replace with the chatID of the user group/channel you want to forward message to
 FORWARD_TO_CHAT_ID = 'your_chat_id_here'
 
+def forwarder_message(update: Update, context: CallbackContext):
+    message = update.message
+    chat_id = update.message.chat_id
+
+    # forward the message
+    forwarded_message = message.forward(FORWARD_TO_CHAT_ID)
+
 updater.start_polling()
