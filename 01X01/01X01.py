06477e362755504767174558c8b4df2bacaaf861
AmirHosein <awir.software@gmail.com>
2023-03-09 18:31:24 +0330

    add forwarder chat id

--- 01X01/01X01.py
+++ 01X01/01X01.py
@@ -7,6 +7,7 @@ from telegram.ext import Updater, CallbackContext, MessageHandler, filters
 updater = Updater(api_token)
 dispatcher = updater.dispatcher
 
-
+# replace with the chatID of the user group/channel you want to forward message to
+FORWARD_TO_CHAT_ID = 'your_chat_id_here'
 
 updater.start_polling()
